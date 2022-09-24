666f26cdcb10879bbbc64db942c2607dd23bb715
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-25 06:54:07 +0900

    Anonymous Login Logic

--- app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
@@ -2,17 +2,27 @@ package com.example.mysololife.auth
 
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.example.auth.LoginActivity
+import com.example.mysololife.MainActivity
 import com.example.mysololife.R
+import com.example.mysololife.auth.JoinActivity
 import com.example.mysololife.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding : ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
 
         super.onCreate(savedInstanceState)
 
@@ -28,5 +38,17 @@ class IntroActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously().addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mysololife/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mysololife/auth/LoginActivity.kt
@@ -1,12 +1,52 @@
-package com.example.mysololife.auth
+package com.example.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.mysololife.MainActivity
 import com.example.mysololife.R
+import com.example.mysololife.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
         setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.loginBtn.setOnClickListener {
+
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(intent)
+
+                    Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_LONG).show()
+
+                } else {
+                    Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_LONG).show()
+
+                }
+            }
+
+
+        }
     }
 }
