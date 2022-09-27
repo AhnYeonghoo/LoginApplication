f7a502db17fd00334ad2ed7017ca721940bd36d9
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-27 16:40:25 +0900

    네비게이션 구현중

--- app/src/main/java/com/example/mysololife/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/mysololife/fragments/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.mysololife.R
 import com.example.mysololife.databinding.FragmentHomeBinding
 
@@ -38,8 +39,19 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
 
         binding.tipTap.setOnClickListener {
-            Toast.makeText(requireContext(), "Clicked", Toast.LENGTH_LONG).show()
+            Toast.makeText(context, "Clicked", Toast.LENGTH_LONG).show()
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
         }
+        binding.talkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+        }
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+        }
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
+        }
+
         return binding.root
 
     }
